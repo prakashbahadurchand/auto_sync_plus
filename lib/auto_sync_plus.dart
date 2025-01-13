9e87026dbd191564d2e0eab9b0e484b2352b4f7d
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2025-01-13 18:58:46 +0545

    by PrakashBahadurChand@gmail.com

--- lib/auto_sync_plus.dart
+++ lib/auto_sync_plus.dart
@@ -13,7 +13,7 @@ class AutoSyncPlus {
   static final AutoSyncPlus _instance = AutoSyncPlus._internal();
   final String _group = 'auto_sync_plus';
   bool logging = false;
-  
+
   factory AutoSyncPlus({bool logging = false}) {
     _instance.logging = logging;
     return _instance;
@@ -21,29 +21,35 @@ class AutoSyncPlus {
   AutoSyncPlus._internal();
 
   void _log(String message) {
-    if (logging) dev.log("[AutoSyncPlus] :: $message");
+    if (logging) {
+      dev.log("# ♻️ [AutoSyncPlus] :: $message");
+    }
   }
 
   /// Checks network connectivity.
   Future<bool> hasInternetAccess() async {
     var connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult.any((ConnectivityResult result) => result != ConnectivityResult.none);
+    return connectivityResult
+        .any((ConnectivityResult result) => result != ConnectivityResult.none);
   }
 
   /// Saves data to cache.
   Future<void> saveToCache(String key, dynamic data) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(key, jsonEncode(data));
-    if (logging) dev.log("[AutoSyncPlus] :: Data saved to cache with key: $key");
+    _log("Data saved to cache with key: $key");
   }
 
   /// Loads data from cache.
-  Future<T?> loadFromCache<T>(String key, T Function(Map<String, dynamic>) fromJson) async {
+  Future<T?> loadFromCache<T>(
+    String key,
+    T Function(Map<String, dynamic>) fromJson,
+  ) async {
     final prefs = await SharedPreferences.getInstance();
     final jsonString = prefs.getString(key);
     if (jsonString == null) return null;
     final Map<String, dynamic> decodedData = jsonDecode(jsonString);
-    if (logging) dev.log("[AutoSyncPlus] :: Data loaded from cache with key: $key");
+    _log("Data loaded from cache with key: $key");
     return fromJson(decodedData);
   }
 
@@ -51,21 +57,22 @@ class AutoSyncPlus {
   Future<String> downloadAndSaveFile(String url) async {
     try {
       final directory = await getApplicationDocumentsDirectory();
-      final filePath = '${directory.path}/auto_sync_plus/${_getUniqueFileNameFromUrl(url)}';
+      final filePath =
+          '${directory.path}/auto_sync_plus/${_getUniqueFileNameFromUrl(url)}';
       final file = File(filePath);
       if (await file.exists()) {
-        if (logging) dev.log("[AutoSyncPlus] :: File already exists at: $filePath");
+        _log("File already exists at: $filePath");
         return filePath;
       }
       final response = await Dio().download(url, filePath);
       if (response.statusCode == 200) {
-        if (logging) dev.log("[AutoSyncPlus] :: File downloaded and saved at: $filePath");
+        _log("File downloaded and saved at: $filePath");
         return filePath;
       } else {
         throw Exception('Failed to download file');
       }
     } catch (e) {
-      if (logging) dev.log("[AutoSyncPlus] :: Error downloading file: $e");
+      _log("Error downloading file: $e");
       return '';
     }
   }
@@ -84,7 +91,8 @@ class AutoSyncPlus {
   /// Gets the saved file path for a given URL.
   Future<String?> getSavedFilePath(String url) async {
     final directory = await getApplicationDocumentsDirectory();
-    final filePath = '${directory.path}/$_group/${_getUniqueFileNameFromUrl(url)}';
+    final filePath =
+        '${directory.path}/$_group/${_getUniqueFileNameFromUrl(url)}';
     return await _fileExists(filePath) ? filePath : null;
   }
 
@@ -101,11 +109,14 @@ class AutoSyncPlus {
       try {
         final data = await apiCall();
         final itemMap = toJson(data);
-        final List<String> urlsToDownload = [];
-        _findUrlsToDownload(itemMap, urlsToDownload, cacheImage, cachePDF);
+        final List<String> urlsToDownload =
+            _findUrlsToDownload(itemMap, cacheImage, cachePDF);
+        _log("Urls to download: $urlsToDownload");
         for (var url in urlsToDownload) {
           final localPath = await downloadAndSaveFile(url);
-          if (localPath.isNotEmpty) _replaceUrlWithLocalPath(itemMap, url, localPath);
+          if (localPath.isNotEmpty) {
+            _replaceUrlWithLocalPath(itemMap, url, localPath);
+          }
         }
         await saveToCache(key, toJson(data));
         return data;
@@ -129,11 +140,14 @@ class AutoSyncPlus {
         try {
           final data = await param.apiCall();
           final itemMap = param.toJson(data);
-          final List<String> urlsToDownload = [];
-          _findUrlsToDownload(itemMap, urlsToDownload, param.cacheImage, param.cachePDF);
+          final List<String> urlsToDownload =
+              _findUrlsToDownload(itemMap, param.cacheImage, param.cachePDF);
+          _log("Urls to download: $urlsToDownload");
           for (var url in urlsToDownload) {
             final localPath = await downloadAndSaveFile(url);
-            if (localPath.isNotEmpty) _replaceUrlWithLocalPath(itemMap, url, localPath);
+            if (localPath.isNotEmpty) {
+              _replaceUrlWithLocalPath(itemMap, url, localPath);
+            }
           }
           await saveToCache(param.key, param.toJson(data));
           completedCalls++;
@@ -148,22 +162,45 @@ class AutoSyncPlus {
     }
   }
 
-  void _findUrlsToDownload(Map<String, dynamic> itemMap, List<String> urlsToDownload, bool cacheImage, bool cachePDF) {
-    itemMap.forEach((key, value) {
-      if (value is String) {
-        if (cacheImage && (value.endsWith('.jpg') || value.endsWith('.jpeg') || value.endsWith('.png'))) urlsToDownload.add(value);
-        if (cachePDF && value.endsWith('.pdf')) urlsToDownload.add(value);
-      } else if (value is Map<String, dynamic>) {
-        _findUrlsToDownload(value, urlsToDownload, cacheImage, cachePDF);
-      } else if (value is List) {
-        for (var item in value) {
-          if (item is Map<String, dynamic>) _findUrlsToDownload(item, urlsToDownload, cacheImage, cachePDF);
-        }
-      }
-    });
+  List<String> _findUrlsToDownload(
+    Map<String, dynamic> itemMap,
+    bool cacheImage,
+    bool cachePDF,
+  ) {
+    final String itemMapString = jsonEncode(itemMap);
+    final List<String> urls = [];
+    final regex =
+        RegExp(r'https?://[^\s,"]+\.(png|jpg|jpeg)', caseSensitive: false);
+
+    // Match all the URLs in the input string
+    final matches = regex.allMatches(itemMapString);
+
+    for (var match in matches) {
+      // Extract the URL from each match and add it to the list
+      _log("Match URL: ${match.group(0)}");
+      urls.add(match.group(0)!);
+    }
+
+    // Do not cache Images if `cacheImage` is false
+    if (!cacheImage) {
+      urls.removeWhere((url) =>
+          url.toLowerCase().endsWith('.jpg') ||
+          url.toLowerCase().endsWith('.jpeg') ||
+          url.toLowerCase().endsWith('.png'));
+    }
+
+    // Do not cache PDFs if `cachePDF` is false
+    if (!cachePDF) {
+      urls.removeWhere((url) => url.toLowerCase().endsWith('.pdf'));
+    }
+
+    _log("Urls to download: $urls");
+
+    return urls;
   }
 
-  void _replaceUrlWithLocalPath(Map<String, dynamic> itemMap, String url, String localPath) {
+  void _replaceUrlWithLocalPath(
+      Map<String, dynamic> itemMap, String url, String localPath) {
     itemMap.forEach((key, value) {
       if (value is String && value == url) {
         itemMap[key] = localPath;
@@ -171,7 +208,9 @@ class AutoSyncPlus {
         _replaceUrlWithLocalPath(value, url, localPath);
       } else if (value is List) {
         for (var item in value) {
-          if (item is Map<String, dynamic>) _replaceUrlWithLocalPath(item, url, localPath);
+          if (item is Map<String, dynamic>) {
+            _replaceUrlWithLocalPath(item, url, localPath);
+          }
         }
       }
     });
@@ -181,11 +220,12 @@ class AutoSyncPlus {
   Future<void> deleteCachedAllPrefs() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.clear();
-    if (logging) dev.log("[AutoSyncPlus] :: All cached preferences deleted");
+    _log("All cached preferences deleted");
   }
 
   /// Deletes all cached files under the `_group` directory with optional flags for selective deletion.
-  Future<void> deleteCachedAllFiles({bool deleteImage = true, bool deletePDF = true}) async {
+  Future<void> deleteCachedAllFiles(
+      {bool deleteImage = true, bool deletePDF = true}) async {
     final directory = await getApplicationDocumentsDirectory();
     final groupDirectory = Directory('${directory.path}/auto_sync_plus');
 
@@ -194,18 +234,75 @@ class AutoSyncPlus {
       for (var file in files) {
         final filePath = file.path;
         final fileExtension = path.extension(filePath).toLowerCase();
-        if ((deleteImage && (fileExtension == '.jpg' || fileExtension == '.jpeg' || fileExtension == '.png')) ||
+        if ((deleteImage &&
+                (fileExtension == '.jpg' ||
+                    fileExtension == '.jpeg' ||
+                    fileExtension == '.png')) ||
             (deletePDF && fileExtension == '.pdf') ||
             (!deleteImage && !deletePDF)) {
           await file.delete();
-          if (logging) dev.log("[AutoSyncPlus] :: Deleted file: $filePath");
+          _log("Deleted file: $filePath");
         }
       }
       if (groupDirectory.listSync().isEmpty) {
         await groupDirectory.delete();
-        if (logging) dev.log("[AutoSyncPlus] :: Deleted directory: ${groupDirectory.path}");
+        _log("Deleted directory: ${groupDirectory.path}");
       }
     }
+  }
+
+  // For Post or Queue Management for Later Sync to Server:
+
+  Future<void> postOrQueueData<T>({
+    required String key,
+    required Future<void> Function() apiCall,
+    required T data,
+    required Map<String, dynamic> Function(T) toJson,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    final queueKey = '${key}_queue';
+
+    try {
+      // Attempt to make the API call
+      await apiCall();
+    } catch (e) {
+      // If the API call fails, add the data to the queue
+      final queue = prefs.getStringList(queueKey) ?? [];
+      queue.add(jsonEncode(toJson(data)));
+      await prefs.setStringList(queueKey, queue);
+    }
+  }
+
+  Future<void> syncQueuedData<T>({
+    required String key,
+    required Future<void> Function(T data) apiCall,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    final queueKey = '${key}_queue';
+    final queue = prefs.getStringList(queueKey) ?? [];
+
+    final remainingQueue = <String>[];
+
+    for (final item in queue) {
+      try {
+        final data = fromJson(jsonDecode(item));
+        await apiCall(data);
+      } catch (e) {
+        // If the API call fails, keep the item in the queue
+        remainingQueue.add(item);
+      }
+    }
+
+    // Update the queue with the remaining items
+    await prefs.setStringList(queueKey, remainingQueue);
+  }
+
+  Future<bool> checkQueueExists(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    final queueKey = '${key}_queue';
+    final queue = prefs.getStringList(queueKey);
+    return queue != null && queue.isNotEmpty;
   }
 }
 
